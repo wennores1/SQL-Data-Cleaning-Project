78f58a995b406ea1350db641ce0e19b507adf3a0
wennores1 <wen_nores@mi.unc.edu.ar>
2026-02-03 20:36:24 -0300

    Add files via upload

---
+++ Exploratory Data Analysis.sql
@@ -0,0 +1,92 @@
+-- exploratory Data Analysis
+
+Select max(total_laid_off), max(percentage_laid_off)
+from layoffs_staging2;
+
+Select *
+from layoffs_staging2
+where percentage_laid_off = 1
+order by total_laid_off desc;
+
+Select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+Select min(`date`), max(`date`)
+from layoffs_staging2;
+
+
+Select industry, sum(total_laid_off)
+from layoffs_staging2
+group by industry
+order by 2 desc;
+
+Select *
+from layoffs_staging2;
+
+Select year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by year(`date`)
+order by 1 desc;
+
+Select stage, sum(total_laid_off)
+from layoffs_staging2
+group by stage
+order by 2 desc;
+
+Select company, avg(total_laid_off) 
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+select substring(`date`, 1, 7) as `month`, sum(total_laid_off)
+from layoffs_staging2
+where substring(`date`, 1, 7) is not null
+group by `month`
+order by 1 asc
+;
+
+with rolling_total as
+(select substring(`date`, 1, 7) as `month`, sum(total_laid_off) as total_off
+from layoffs_staging2
+where substring(`date`, 1, 7) is not null
+group by `month`
+order by 1 asc
+)
+select `month`, total_off,
+sum(total_off) over(order by `month`) as rolling_total
+from rolling_total;
+
+
+Select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+Select company, year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by company, year(`date`)
+order by company asc;
+
+Select company, year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by company, year(`date`)
+order by 3 desc;
+
+
+with Company_year (company, years, total_laid_off) as
+(
+Select company, year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by company, year(`date`)
+), company_year_rank as
+(select *, dense_rank() over (partition by years order by total_laid_off desc) as ranking
+from Company_year
+where years is not null
+)
+select *
+from company_year_rank
+where ranking <= 5;
+
+
